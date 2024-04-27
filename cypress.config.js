547599313d7996e71ac8350c6074ce017b21d437
marianachoratto <marianachoratto@gmail.com>
2024-04-27 20:16:46 -0300

    fix: consertando bug da barra de pesquisa

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://rarocrud-frontend-88984f6e4454.herokuapp.com/users",
+    baseUrl: "https://rarocrud-frontend-88984f6e4454.herokuapp.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
