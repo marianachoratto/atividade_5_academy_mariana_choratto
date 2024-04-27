7e1bd9403c3244e459336ef2f0c2f8e60dec4827
marianachoratto <marianachoratto@gmail.com>
2024-04-27 20:15:27 -0300

    add lista de usuarios e page objects

--- cypress/e2e/listarUsuarios.cy.js
+++ cypress/e2e/listarUsuarios.cy.js
@@ -1,19 +1,43 @@
 import { faker } from "@faker-js/faker";
+import { PaginaPrincipal } from "../support/pages/paginaPrincipal";
 
 describe("Listar usuários", () => {
   beforeEach(() => {
-    cy.visit("");
+    cy.visit("/users");
   });
 
-  it("Deve verificar se existe um link para a lista de usuários", () => {
-    cy.visit("https://example.cypress.io");
+  let pgPrincipal = new PaginaPrincipal();
+
+  it("Deve retornar listas de usuários", () => {
+    cy.get(pgPrincipal.divListaDeUsuarios).should("be.visible");
   });
 
-  it("Deve retornar listas de usuários", () => {});
+  it("A lista deve trazer 6 usuários cadastrados", () => {
+    cy.log(cy.get(pgPrincipal.divListaDeUsuarios));
+
+    cy.get(pgPrincipal.divListaDeUsuarios).should("have.length", 6);
+  });
+
+  it.only("Teste mockado com 12 usuários e 2 páginas", function () {
+    cy.fixture("listaDeUsuariosMock.json").then((arquivo) => {
+      cy.intercept("GET", "api/v1/users", {
+        statusCode: 200,
+        body: arquivo,
+      }).as("usuarioMockado");
+    });
+    cy.wait("@usuarioMockado");
+    cy.get("[id='userData']").should("have.length", 6);
+    cy.get(pgPrincipal.liTextoPaginas).should("have.text", "1 de 2");
+  });
 
   it("Deve apertar o botão ver detalhes", () => {});
 
-  it("Atualizar página inicial", () => {});
+  it("Link para atualizar página inicial", () => {
+    cy.get(pgPrincipal.anchorRaro).should("be.visible");
+  });
+
   // último teste
-  it("Ir para cadastrar usuário", () => {});
+  it("Ir para cadastro de usuário", () => {
+    cy.get(pgPrincipal.anchorVoltar).should("be.visible");
+  });
 });
