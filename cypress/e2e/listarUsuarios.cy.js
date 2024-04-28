bc015b377f0e709be51924ed08633a84396c6073
marianachoratto <marianachoratto@gmail.com>
2024-04-27 22:21:09 -0300

    add novos testes de lista de usuário

--- cypress/e2e/listarUsuarios.cy.js
+++ cypress/e2e/listarUsuarios.cy.js
@@ -1,4 +1,3 @@
-import { faker } from "@faker-js/faker";
 import { PaginaPrincipal } from "../support/pages/paginaPrincipal";
 
 describe("Listar usuários", () => {
@@ -18,16 +17,42 @@ describe("Listar usuários", () => {
     cy.get(pgPrincipal.divListaDeUsuarios).should("have.length", 6);
   });
 
-  it.only("Teste mockado com 12 usuários e 2 páginas", function () {
-    cy.fixture("listaDeUsuariosMock.json").then((arquivo) => {
-      cy.intercept("GET", "api/v1/users", {
-        statusCode: 200,
-        body: arquivo,
-      }).as("usuarioMockado");
-    });
+  it("Quando vier menos de 6 usuários aparecerá menos de 6 elementos de usuários", () => {
+    cy.intercept("GET", "api/v1/users", {
+      statusCode: 200,
+      fixture: "lista2Usuarios.json",
+    }).as("pequenaListaDeUsuarios");
+
+    cy.wait("@pequenaListaDeUsuarios");
+    cy.get("[id='userData']").should("have.length", 2);
+    cy.get(pgPrincipal.liTextoPaginas)
+      .invoke("text")
+      .should("be.equal", "1 de 1");
+  });
+
+  it("Teste mockado com 12 usuários e 2 páginas", function () {
+    cy.intercept("GET", "api/v1/users", {
+      statusCode: 200,
+      fixture: "listaDeUsuariosMock.json",
+    }).as("usuarioMockado");
+
     cy.wait("@usuarioMockado");
     cy.get("[id='userData']").should("have.length", 6);
     cy.get(pgPrincipal.liTextoPaginas).should("have.text", "1 de 2");
+  });
+
+  it.only("Não aparecerá os elementos dos usuários quando não houver nenhum usuário", () => {
+    cy.intercept("GET", "api/v1/users", {
+      statusCode: 200,
+    }).as("noUsers");
+
+    cy.wait("@noUsers");
+    cy.contains(
+      "h3",
+      "Ops! Não existe nenhum usuário para ser exibido."
+    ).should("be.visible");
+    cy.contains("p", "Cadastre um novo usuário").should("be.visible");
+    cy.get(pgPrincipal.divDadosUsuarios).should("have.length", 0);
   });
 
   it("Deve apertar o botão ver detalhes", () => {});
